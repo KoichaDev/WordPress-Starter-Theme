79ed23817925ae053fe026001b381b998a555f86
Khoi Hoang <khoii87@gmail.com>
2021-02-26 00:21:57 +0100

    Fix: 🐛 Corrected path to ignoring files for plugins and theme

--- wp-content/themes/_theme_name/inc/functions/enqueue_scripts_and_styles.php
+++ wp-content/themes/_theme_name/inc/functions/enqueue_scripts_and_styles.php
@@ -1,8 +1,17 @@
 <?php 
 
-function theme_scripts_and_styles() {
-    wp_enqueue_style( 'working-group-two-style-sheet', get_template_directory_uri() . '/dist/index.css' );
-    wp_enqueue_script( 'working-group-two-javascript', get_template_directory_uri() . '/dist/index.js' );
+function _theme_name_script_defer( $tag, $handle, $src ) {
+    if ( 'index' !== $handle ) {
+		return $tag;
+	}
+	return str_replace( ' src', ' defer src', $tag ); // defer the script
+} 
+
+function _theme_name_scripts_and_styles() {
+    wp_enqueue_style( '_theme_name-style-sheet', get_template_directory_uri() . '/dist/index.css' );
+    wp_enqueue_script( '_theme_name-javascript', get_template_directory_uri() . '/dist/index.js' );
 }
 
-add_action( 'wp_enqueue_scripts', 'theme_scripts_and_styles' );
+
+add_filter( 'script_loader_tag', '_theme_name_script_defer', 10, 3 );
+add_action( 'wp_enqueue_scripts', '_theme_name_scripts_and_styles' );
--- wp-content/themes/_theme_name/index.php
+++ wp-content/themes/_theme_name/index.php
@@ -1,3 +1,3 @@
 <?php get_header(); ?> 
-
+asiudhuisahduiashd
 <?php get_footer(); ?>
