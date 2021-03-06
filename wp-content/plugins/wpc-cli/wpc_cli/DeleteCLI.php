4d37a52b475351c63725646cdc07c706445c8479
Khoi Hoang <khoii87@gmail.com>
2021-03-06 13:53:42 +0100

    Add: ✨ Customized WP CLI plugin
    This plugin is used for our own customized WP CLI.
    For now, this we have created a WP CLI for creating x amount of dummy posts.
    
    The CLI:
    
    wp create posts --amount=n
    
    ** Note: 'n' stands for number

---
+++ wp-content/plugins/wpc-cli/wpc_cli/DeleteCLI.php
@@ -0,0 +1,65 @@
+<?php
+
+class DeleteCLI {
+    public $post_id;
+    public $post_title;
+    public $post_content;
+    public $post_excerpt;
+
+    public function posts($args, $assoc_args) {
+        $amount = $assoc_args['amount'];
+
+        $home_type_terms = get_terms([
+            'taxonomy'      => 'category',
+            'hide_empty'    => false, // We want to bring all kind of 'category, even if it's not a post assocated with them
+        ]);
+
+        $home_type_ids = array_map(function ($home_type) {
+            return $home_type -> term_id;
+        }, $home_type_terms);
+
+        $faker = Faker\Factory::create();
+        $progress = \WP_CLI\Utils\make_progress_bar( 'Generating post(s)', $amount );
+        
+        for ($i=0; $i < $amount; $i++) {
+
+            $this -> post_title   = $faker -> firstName . ' ' . $faker -> lastName;
+            $this -> post_content = $faker -> sentence($nbWords = rand(200, 500), $variableNbWords = true);
+            $this -> post_excerpt = $faker -> sentence($nbWords = rand(50, 100), $variableNbWords = true);
+            
+            $this -> post_id = wp_insert_post([
+                'post_type'     => 'post',
+                'post_status'   => 'publish',
+                'post_title'    => $this -> post_title,
+                'post_content'  => $this -> post_content,
+                'post_excerpt'  => $this -> post_excerpt,
+            ]);
+
+            $args = [
+                'redirection' => 0 // Don't follow any 300-range redirections
+            ];
+                
+            $response = wp_remote_get( 'https://picsum.photos/640/360', $args );
+
+            $image_url = $response['headers']['location'];
+            
+            $feature_image = media_sideload_image(
+                $image_url,
+                $this  -> post_id,
+                $this -> post_title,
+                'id'
+            );
+            
+            // wp_update_post($faker -> numberBetween(2, 10), array_rand( array_flip( $random_image ) ), $floorplan);
+            set_post_thumbnail($this -> post_id, $feature_image);
+
+            $progress -> tick();
+        }
+
+        $progress -> finish();
+
+        WP_CLI::success($amount . ' random posts generated!');
+    }
+}
+
+WP_CLI::add_command('delete', 'DeleteCLI' );
