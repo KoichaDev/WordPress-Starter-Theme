bc58e7c1c4aa9cc2f4fc162401e40c8a58a6ad97
Krachi Crunch <khoii87@gmail.com>
2021-03-08 18:18:58 +0100

    Add: ✨ Untrash all post based on post type
    CLI:
    wp update untrash_all_posts --post_type=<post_type> --[<publish>|<draft>]

--- wp-content/plugins/wpc-cli/wpc_cli/UpdateCLI.php
+++ wp-content/plugins/wpc-cli/wpc_cli/UpdateCLI.php
@@ -14,32 +14,35 @@ class UpdateCLI {
 
         $this -> progress = \WP_CLI\Utils\make_progress_bar( 'Updating...', $status );
 
-        $posts = get_posts([
-            'posts_per_page'    => -1,
-            'post_status'       => 'trash',
-            'post_type'         => 'post',
-            'fields'            => 'ids', // Only get post ID's
-        ]);
+        if( post_type_exists( $assoc_args['post_type'] ) ) {
+            $posts = get_posts([
+                'posts_per_page'    => -1,
+                'post_status'       => 'trash',
+                'post_type'         => $assoc_args['post_type'],
+                'fields'            => 'ids', // Only get post ID's
+            ]);
 
-        if(isset( $assoc_args['publish'])) {
-            foreach ( $posts as $post ) {
-                wp_untrash_post( $post); // Set to False if you want to send them to Trash.   
-                wp_publish_post( $post );
-                $this -> progress -> tick();
+            if( isset( $assoc_args['publish'] ) ) {
+                foreach ( $posts as $post ) {
+                    wp_untrash_post( $post); // Set to False if you want to send them to Trash.   
+                    wp_publish_post( $post );
+                    $this -> progress -> tick();
+                }
+                $this -> progress -> finish();
+                WP_CLI::success('Successfully published and untrashed the post(s)');
+            } else if(isset( $assoc_args['draft'])) {
+                foreach ( $posts as $post ) {
+                    wp_untrash_post( $post); // Set to False if you want to send them to Trash.   
+                    $progress -> tick();
+                }
+                $this -> progress -> finish();
+                WP_CLI::success('Successfully drafted and untrashed the post(s)');
+            } else {
+                WP_CLI::error('Invalid argument. wp update --help to retrieve list');
             }
-            $this -> progress -> finish();
-            WP_CLI::success('Successfully published and untrashed the post(s)');
-        } else if(isset( $assoc_args['draft'])) {
-            foreach ( $posts as $post ) {
-                wp_untrash_post( $post); // Set to False if you want to send them to Trash.   
-                $progress -> tick();
-            }
-            $this -> progress -> finish();
-            WP_CLI::success('Successfully drafted and untrashed the post(s)');
         } else {
-            WP_CLI::error('Invalid argument. wp update --help to retrieve list');
-        }
-
+            WP_CLI::error('Post Type does not exist!');
+        }      
         add_action( 'trash_to_publish',  'untrash_all_posts', 10, 1 );
     }
 }
