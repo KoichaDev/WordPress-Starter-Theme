a93a3577d315479274119f55dd4594f3f85fe61a
Krachi Crunch <khoii87@gmail.com>
2021-03-10 14:36:24 +0100

    Add: ✨ Added post_type for creating random posts:
    CLI:
    
    wp create posts --post_type=<post_type> --total=<n>

--- wp-content/plugins/wpc-cli/wpc_cli/CreateCLI.php
+++ wp-content/plugins/wpc-cli/wpc_cli/CreateCLI.php
@@ -9,56 +9,109 @@ class CreateData {
     public function posts($args, $assoc_args) {
         $amount = $assoc_args['total'];
 
-        $home_type_terms = get_terms([
-            'taxonomy'      => 'category',
-            'hide_empty'    => false, // We want to bring all kind of 'category, even if it's not a post assocated with them
-        ]);
-
-        $home_type_ids = array_map(function ($home_type) {
-            return $home_type -> term_id;
-        }, $home_type_terms);
-
-        $faker = Faker\Factory::create();
-        $progress = \WP_CLI\Utils\make_progress_bar( 'Generating post(s)', $amount );
-        
-        for ($i=0; $i < $amount; $i++) {
-
-            $this -> post_title   = $faker -> firstName . ' ' . $faker -> lastName;
-            $this -> post_content = $faker -> sentence($nbWords = rand(200, 500), $variableNbWords = true);
-            $this -> post_excerpt = $faker -> sentence($nbWords = rand(50, 100), $variableNbWords = true);
-            
-            $this -> post_id = wp_insert_post([
-                'post_type'     => 'post',
-                'post_status'   => 'publish',
-                'post_title'    => $this -> post_title,
-                'post_content'  => $this -> post_content,
-                'post_excerpt'  => $this -> post_excerpt,
+        if($assoc_args['post_type']) {
+            $home_type_terms = get_terms([
+                'taxonomy'      => 'category',
+                'hide_empty'    => false, // We want to bring all kind of 'category, even if it's not a post assocated with them
             ]);
 
-            $args = [
-                'redirection' => 0 // Don't follow any 300-range redirections
-            ];
+            $home_type_ids = array_map(function ($home_type) {
+                return $home_type -> term_id;
+            }, $home_type_terms);
+
+            $faker = Faker\Factory::create();
+            $progress = \WP_CLI\Utils\make_progress_bar( 'Generating post(s)', $amount );
+            
+            for ($i=0; $i < $amount; $i++) {
+
+                $this -> post_title   = $faker -> firstName . ' ' . $faker -> lastName;
+                $this -> post_content = $faker -> sentence($nbWords = rand(200, 500), $variableNbWords = true);
+                $this -> post_excerpt = $faker -> sentence($nbWords = rand(50, 100), $variableNbWords = true);
                 
-            $response = wp_remote_get( 'https://picsum.photos/640/360', $args );
+                $this -> post_id = wp_insert_post([
+                    'post_type'     => $assoc_args['post_type'],
+                    'post_status'   => 'publish',
+                    'post_title'    => $this -> post_title,
+                    'post_content'  => $this -> post_content,
+                    'post_excerpt'  => $this -> post_excerpt,
+                ]);
 
-            $image_url = $response['headers']['location'];
-            
-            $feature_image = media_sideload_image(
-                $image_url,
-                $this  -> post_id,
-                $this -> post_title,
-                'id'
-            );
-            
-            // wp_update_post($faker -> numberBetween(2, 10), array_rand( array_flip( $random_image ) ), $floorplan);
-            set_post_thumbnail($this -> post_id, $feature_image);
+                $args = [
+                    'redirection' => 0 // Don't follow any 300-range redirections
+                ];
+                    
+                $response = wp_remote_get( 'https://picsum.photos/640/360', $args );
 
-            $progress -> tick();
+                $image_url = $response['headers']['location'];
+                
+                $feature_image = media_sideload_image(
+                    $image_url,
+                    $this  -> post_id,
+                    $this -> post_title,
+                    'id'
+                );
+                
+                // wp_update_post($faker -> numberBetween(2, 10), array_rand( array_flip( $random_image ) ), $floorplan);
+                set_post_thumbnail($this -> post_id, $feature_image);
+
+                $progress -> tick();
+            }
+
+            $progress -> finish();
+
+            WP_CLI::success($amount . ' random posts generated!');
+        } else {
+            $home_type_terms = get_terms([
+            'taxonomy'      => 'category',
+            'hide_empty'    => false, // We want to bring all kind of 'category, even if it's not a post assocated with them
+            ]);
+
+            $home_type_ids = array_map(function ($home_type) {
+                return $home_type -> term_id;
+            }, $home_type_terms);
+
+            $faker = Faker\Factory::create();
+            $progress = \WP_CLI\Utils\make_progress_bar( 'Generating post(s)', $amount );
+            
+            for ($i=0; $i < $amount; $i++) {
+
+                $this -> post_title   = $faker -> firstName . ' ' . $faker -> lastName;
+                $this -> post_content = $faker -> sentence($nbWords = rand(200, 500), $variableNbWords = true);
+                $this -> post_excerpt = $faker -> sentence($nbWords = rand(50, 100), $variableNbWords = true);
+                
+                $this -> post_id = wp_insert_post([
+                    'post_type'     => 'post',
+                    'post_status'   => 'publish',
+                    'post_title'    => $this -> post_title,
+                    'post_content'  => $this -> post_content,
+                    'post_excerpt'  => $this -> post_excerpt,
+                ]);
+
+                $args = [
+                    'redirection' => 0 // Don't follow any 300-range redirections
+                ];
+                    
+                $response = wp_remote_get( 'https://picsum.photos/640/360', $args );
+
+                $image_url = $response['headers']['location'];
+                
+                $feature_image = media_sideload_image(
+                    $image_url,
+                    $this  -> post_id,
+                    $this -> post_title,
+                    'id'
+                );
+                
+                // wp_update_post($faker -> numberBetween(2, 10), array_rand( array_flip( $random_image ) ), $floorplan);
+                set_post_thumbnail($this -> post_id, $feature_image);
+
+                $progress -> tick();
+            }
+
+            $progress -> finish();
+
+            WP_CLI::success($amount . ' random posts generated!');
         }
-
-        $progress -> finish();
-
-        WP_CLI::success($amount . ' random posts generated!');
     }
 }
 
