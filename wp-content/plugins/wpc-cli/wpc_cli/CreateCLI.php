b6a19fa9a58afc12a81510e7f4ac1412dbb140dd
Krachi Crunch <khoii87@gmail.com>
2021-03-10 14:45:02 +0100

    Add: ✨ Creating random single post with CPT (optional)
    CLI:
    
    - wp create post
    - wp create post --post_type

--- wp-content/plugins/wpc-cli/wpc_cli/CreateCLI.php
+++ wp-content/plugins/wpc-cli/wpc_cli/CreateCLI.php
@@ -5,6 +5,95 @@ class CreateData {
     public $post_title;
     public $post_content;
     public $post_excerpt;
+    
+      public function post($args, $assoc_args) {
+
+        if($assoc_args['post_type']) {
+
+            $faker = Faker\Factory::create();
+            $progress = \WP_CLI\Utils\make_progress_bar( 'Generating single post', $amount );
+
+            $this -> post_title   = $faker -> firstName . ' ' . $faker -> lastName;
+            $this -> post_content = $faker -> sentence($nbWords = rand(200, 500), $variableNbWords = true);
+            $this -> post_excerpt = $faker -> sentence($nbWords = rand(50, 100), $variableNbWords = true);
+            
+            $this -> post_id = wp_insert_post([
+                'post_type'     => $assoc_args['post_type'],
+                'post_status'   => 'publish',
+                'post_title'    => $this -> post_title,
+                'post_content'  => $this -> post_content,
+                'post_excerpt'  => $this -> post_excerpt,
+            ]);
+
+            $args = [
+                'redirection' => 0 // Don't follow any 300-range redirections
+            ];
+                
+            $response = wp_remote_get( 'https://picsum.photos/640/360', $args );
+
+            $image_url = $response['headers']['location'];
+            
+            $feature_image = media_sideload_image(
+                $image_url,
+                $this  -> post_id,
+                $this -> post_title,
+                'id'
+            );
+            
+            // wp_update_post($faker -> numberBetween(2, 10), array_rand( array_flip( $random_image ) ), $floorplan);
+            set_post_thumbnail($this -> post_id, $feature_image);
+
+            $progress -> tick();
+            
+            $progress -> finish();
+
+            WP_CLI::success('A random post generated!');
+        } else {
+            $home_type_ids = array_map(function ($home_type) {
+                return $home_type -> term_id;
+            }, $home_type_terms);
+
+            $faker = Faker\Factory::create();
+            $progress = \WP_CLI\Utils\make_progress_bar( 'Generating single post', $amount );
+            
+            $this -> post_title   = $faker -> firstName . ' ' . $faker -> lastName;
+            $this -> post_content = $faker -> sentence($nbWords = rand(200, 500), $variableNbWords = true);
+            $this -> post_excerpt = $faker -> sentence($nbWords = rand(50, 100), $variableNbWords = true);
+            
+            $this -> post_id = wp_insert_post([
+                'post_type'     => 'post',
+                'post_status'   => 'publish',
+                'post_title'    => $this -> post_title,
+                'post_content'  => $this -> post_content,
+                'post_excerpt'  => $this -> post_excerpt,
+            ]);
+
+            $args = [
+                'redirection' => 0 // Don't follow any 300-range redirections
+            ];
+                
+            $response = wp_remote_get( 'https://picsum.photos/640/360', $args );
+
+            $image_url = $response['headers']['location'];
+            
+            $feature_image = media_sideload_image(
+                $image_url,
+                $this  -> post_id,
+                $this -> post_title,
+                'id'
+            );
+            
+            // wp_update_post($faker -> numberBetween(2, 10), array_rand( array_flip( $random_image ) ), $floorplan);
+            set_post_thumbnail($this -> post_id, $feature_image);
+
+            $progress -> tick();
+            
+
+            $progress -> finish();
+
+            WP_CLI::success( 'A random post generated!');
+        }
+    }
 
     public function posts($args, $assoc_args) {
         $amount = $assoc_args['total'];
