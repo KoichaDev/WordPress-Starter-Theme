af08479dc85b8a140edad8fe0770735d47212f03
Krachi Crunch <khoii87@gmail.com>
2021-03-23 10:58:53 +0100

    Add: ✨ Custome Image Sizes for WP

--- wp-content/themes/_theme_name/functions.php
+++ wp-content/themes/_theme_name/functions.php
@@ -3,6 +3,8 @@ include_once('inc/helpers/auto_load_files.php');
 
 include_once('inc/functions/auto_load_cpt.php');
 
+include_once('inc/functions/custom_image_sizes.php');
+
 include_once('inc/functions/enqueue_scripts_and_styles.php');
 
 include_once('inc/functions/feature_img.php');
