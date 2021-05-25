513b02c42dae8f072c7a0d16650af007d2eddd72
Khoi Hoang <khoii87@gmail.com>
2021-05-25 14:43:18 +0200

    Add: ✨ Callback function for generating dummy text and taking attributes number of posts and post Categories

--- wp-content/plugins/_theme_name-gutenberg-blocks/plugin.php
+++ wp-content/plugins/_theme_name-gutenberg-blocks/plugin.php
@@ -18,10 +18,28 @@ include_once('inc/activate_alignment_block.php');
 function themename_blocks_register() {
     themename_register_block_type('dummy-text');
     themename_register_block_type('hello-world');
-    themename_register_block_type('latest-posts');
     themename_register_block_type('rich-text');
     themename_register_block_type('team-members');
     themename_register_block_type('team-member');
+
+    $attributes = [
+        // This callback will be responsible for the function to display the dynamic Content
+        'render_callback'   => 'themename_blocks_render_latests_posts_block',
+        'attributes' => [
+            'numberOfPosts' => [
+                'type'      => 'number',
+                'default'   => 5
+            ],
+            'postsCategories'   => [
+                'type'  => 'string',
+            ]
+        ]
+    ];
+    themename_register_block_type('latest-posts', $attributes);
 }
 
 add_action('init', 'themename_blocks_register');
+
+function themename_blocks_render_latests_posts_block($attributes) {
+    return '<p>ijioasdjioasjod</p>';
+}
