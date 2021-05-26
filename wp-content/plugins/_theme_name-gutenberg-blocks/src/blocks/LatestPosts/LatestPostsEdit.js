4a6e6e9d6c5c73e622d6c67ccc6ff16d1025152d
Krachi Crunch <khoii87@gmail.com>
2021-05-26 14:15:21 +0200

    Add: ✨ Getting all posts in Editor by using Javascript

--- wp-content/plugins/_theme_name-gutenberg-blocks/src/blocks/LatestPosts/LatestPostsEdit.js
+++ wp-content/plugins/_theme_name-gutenberg-blocks/src/blocks/LatestPosts/LatestPostsEdit.js
@@ -1,10 +1,32 @@
 const { Component } = wp.element;
+const { __ } = wp.i18n;
 const { withSelect } = wp.data; // Using the WP Redux
 
 class LatestPostsEdit extends Component {
   render() {
+    const { posts, className } = this.props;
     console.log(this.props);
-    return <p>akdopaskopd</p>;
+    return (
+      <>
+        {posts && posts.length > 0 ? (
+          <ul className={className}>
+            {posts.map((post) => (
+              <li key={post.id}>
+                <a href={post.link} target='_blank' rel='noopener noreferrer'>
+                  {post.title.rendered}
+                </a>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div>
+            {posts
+              ? __('No WP Posts found', 'themename-blocks')
+              : __('Loading...', 'themename-blocks')}
+          </div>
+        )}
+      </>
+    );
   }
 }
 
@@ -13,9 +35,9 @@ export default withSelect((select, props) => {
   const { numberOfPosts } = attributes;
   // The object below is taken from the WP Rest API arguments you can send
   // https://developer.wordpress.org/rest-api/reference/posts/#arguments
-  let query = { per_page: numberOfPosts };
-
   return {
-    post: select('core').getEntityRecords('postType', 'post', { query }),
+    posts: select('core').getEntityRecords('postType', 'post', {
+      per_page: numberOfPosts, // Getting all the post that exist on the WP database
+    }),
   };
 })(LatestPostsEdit);
